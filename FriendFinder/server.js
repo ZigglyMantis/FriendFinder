b40f8301740c6a48275b4f9dffe9ddcc52fc5ecf
Zygimantas Macijauskas <zigglymantis@gmail.com>
2019-11-16 11:28:58 -0500

    added basic files and created server.js

---
+++ FriendFinder/server.js
@@ -0,0 +1,35 @@
+//===================================================
+// Dependencies
+// NPM packages that allow more useful functionality
+//===================================================
+
+const express = require("express")
+
+//===================================================
+// Configure Express Server
+//===================================================
+
+// creating an 'express' server
+var app = express();
+
+// sets up the port the server will use and listen on
+var PORT = process.env.PORT || 8080;
+
+// allows express to handle data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//====================================================
+// Router with routes
+//====================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//====================================================
+// Listener starts the server
+//====================================================
+
+app.listen(PORT, function() {
+    console.log("Server running on port: " + PORT);
+});
