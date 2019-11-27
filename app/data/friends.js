2d71a6fbba09f1c8ac7e174fc3a8dd50a5153222
Zygimantas Macijauskas <zigglymantis@gmail.com>
2019-11-26 21:03:42 -0500

    changed the way the friendArray handles answers

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,21 +2,14 @@
 // currently set to an empty array
 var friendArray = [
     {
-        name: "",
+        name: "Test",
         photo: "",
-        a1: "",
-        a2: "",
-        a3: "",
-        a4: "",
-        a5: "",
-        a6: "",
-        a7: "",
-        a8: "",
-        a9: "",
-        a10: ""
+        ans: []
     }
 ];
-// exporting the friend array to be used in the app.
-module.exports = friendArray;
 
 //there would be a whole bunch of functions here to add and compare the scores of the users.
+var ansArray = 
+console.log(ansArray)
+// exporting the friend array to be used in the app.
+module.exports = friendArray;
